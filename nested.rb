e2b9750166ddc86e84b64df7e427b15f2eced001
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 21:20:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:language][0]
+programmer_hash[:dennis_ritchie][:language]
 end
 
 def adding_matz
